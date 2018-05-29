82d4e22c11afa67e950d65603dc37cc3ce698aac
Niklas Heist <niklas.heist@online.de>
2018-05-29 15:01:43 +0200

    messages migration fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :user
-  belongs_to :recepient, class_name: "User", foreign_key: "recepient_id"
+  belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
 end
--- db/migrate/20180529125629_add_references_to_messages.rb
+++ db/migrate/20180529125629_add_references_to_messages.rb
@@ -1,6 +1,6 @@
 class AddReferencesToMessages < ActiveRecord::Migration[5.2]
   def change
     add_reference :messages, :user, foreign_key: true
-    add_reference :messages, :recepient, foreign_key: { to_table: :users }
+    add_reference :messages, :recipient, foreign_key: { to_table: :users }
   end
 end
