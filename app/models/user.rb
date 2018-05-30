c35a15b5f839e53588df5e350895dec7a8d7d4a1
Kai Kuenne <Mail@kaikuenne.com>
2018-05-30 17:17:11 +0200

    Prepared FB user login to fetch user_friends once approved by FB

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ApplicationRecord
       user.picture = URI.parse(auth.info.image)
       user.password = Devise.friendly_token[0,20]
       user.friends = auth.user_friends
-      raise
       # user_friends should work after fb approves
     end
   end
