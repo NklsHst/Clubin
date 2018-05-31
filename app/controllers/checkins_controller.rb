8fa34d72e026b82bcdc3bce44f2c8196afbd528e
Kai Kuenne <Mail@kaikuenne.com>
2018-05-31 14:30:38 +0200

    Implemented basic checkin functionality and geolocation fetch

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,4 +1,25 @@
-class CheckInsController < ApplicationController
+class CheckinsController < ApplicationController
+  before_action :set_location, only: [:new, :create]
 
+  def new
+    @checkin = CheckIn.new
+  end
+
+  def create
+    @checkin = CheckIn.new(checkin_params)
+    @checkin.location = @location
+    @checkin.user = current_user
+    @checkin.save
+  end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def checkin_params
+    params.require(:check_in).permit(:latitude, :longitude)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
   # root to: 'locations#index', as: :locations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-#Routes for loaction controller
+#Routes for location controller
 get '/locations', to: 'locations#index', as: :locations
 get '/locations/atmosphere', to: 'locations#index_atmosphere', as: :index_atmosphere
 get '/locations/queue', to: 'locations#index_queue', as: :index_queue
 get '/locations/:id', to: 'locations#show', as: :location
 
 #Routes for checkin controller
-get '/locations/:id/checkin-new', to: 'checkins#new', as: :checkins_new
-post '/locations/:id/checkin-create', to: 'checkins#create', as: :checkins_create
+get '/locations/:id/checkin', to: 'checkins#new', as: :checkins_new
+post '/locations/:id/checkin/create', to: 'checkins#create', as: :checkins_create
 get '/checkins/friends', to: 'checkins#index_friends', as: :checkins_friends
 get '/checkins/partypeople', to: 'checkins#index_partypeople', as: :checkins_partypeople
 get '/locations/:id/checkins', to: 'checkins#index_locations', as: :checkins_location
