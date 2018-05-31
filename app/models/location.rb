fe09139c277f5f8c23f446c211f2744c2c92cd49
Niklas Heist <niklas.heist@online.de>
2018-05-31 11:56:56 +0200

    checkin function

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,6 @@
 class Location < ApplicationRecord
   has_many :check_ins
+  has_many :users, through: :check_ins
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
@@ -16,5 +17,14 @@ class Location < ApplicationRecord
   def calculate_average_queue
     check_ins = self.check_ins
     check_ins.pluck(:queue_rating).sum/check_ins.length
+  end
+
+  def checkedin_users
+    check_ins = self.check_ins
+    users = []
+    check_ins.each do |check_in|
+      users << check_in.user_id
+    end
+    users
   end
 end
