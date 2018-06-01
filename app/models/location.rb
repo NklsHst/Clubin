12181bd8ac04ebedda9e3019c8c753d67653b520
Niklas Heist <niklas.heist@online.de>
2018-06-01 12:52:24 +0200

    locations controller show function

--- app/models/location.rb
+++ app/models/location.rb
@@ -9,39 +9,37 @@ class Location < ApplicationRecord
 
 
   def calculate_average_atmosphere
-    check_ins = self.check_ins
-    check_ins.pluck(:atmosphere_rating).sum/check_ins.length
-
+    check_ins.pluck(:atmosphere_rating).compact.sum / check_ins.count
   end
 
   def calculate_average_queue
-    check_ins = self.check_ins
-    check_ins.pluck(:queue_rating).sum/check_ins.length
+    check_ins.pluck(:queue_rating).compact.sum / check_ins.count
   end
 
-  def checkedin_users
-    check_ins = self.check_ins
-    users = []
-    check_ins.each do |check_in|
-      users << check_in.user_id
-      # use next line instead of the one above for time logic (set the time frame for checkins to 3 hours)
-      # users << check_in.user_id && Time.nom.utc - check_in.created_at < 1080
-    end
-    users
-  end
+  # def checkedin_users
+  #   check_ins
+  #   users = []
+  #   check_ins.each do |check_in|
+  #     users << User.where(id: check_in.user_id)
+  #     # use next line instead of the one above for time logic (set the time frame for checkins to 3 hours)
+  #     # users << check_in.user_id && Time.nom.utc - check_in.created_at < 1080
+  #   end
+  #   users
+  # end
 
-  def checkedin_friends
-    check_ins = self.check_ins
-    user_partner_ids = []
-    checkedin_friends = []
-    current_user.friendships.each do |friendship|
-      user_partner_ids << friendship.partner_id
-    end
-    check_ins.each do |check_in|
-      checkedin_friends << check_in.user_id if user_partner_ids.include? check_in.user_id
-      # use next line instead of the one above for time logic (set the time frame for checkins to 3 hours)
-      # checkedin_friends << check_in.user_id if user_partner_ids.include? check_in.user_id && Time.nom.utc - check_in.created_at < 1080
-    end
-    checkedin_friends
-  end
+  # def checkedin_friends
+    # users.joins(:friendships).where('friendships.user_id = ?', current_user.id)
+    # check_ins = self.check_ins
+    # user_partner_ids = []
+    # checkedin_friends = []
+    # current_user.friendships.each do |friendship|
+    #   user_partner_ids << friendship.partner_id
+    # end
+    # check_ins.each do |check_in|
+    #   checkedin_friends << check_in.user_id if user_partner_ids.include? check_in.user_id
+    #   # use next line instead of the one above for time logic (set the time frame for checkins to 3 hours)
+    #   # checkedin_friends << check_in.user_id if user_partner_ids.include? check_in.user_id && Time.nom.utc - check_in.created_at < 1080
+    # end
+    # checkedin_friends
+  # end
 end
