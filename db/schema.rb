536d0d5386e58cc7a88f2d13989772b43941e7a6
Niklas Heist <niklas.heist@online.de>
2018-06-06 11:52:17 +0200

    seed part 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_31_161040) do
+ActiveRecord::Schema.define(version: 2018_06_06_084503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 2018_05_31_161040) do
     t.string "provider"
     t.string "uid"
     t.string "friends"
+    t.string "about_me"
+    t.string "favourite_club"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
