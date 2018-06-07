b3ac3e616ce27e2d70af101fe9ad52484c16cd2c
Niklas Heist <niklas.heist@online.de>
2018-06-07 20:46:04 +0200

    meta-data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  def default_url_options
+  { host: ENV["www.clubin.app"] || "localhost:3000" }
+  end
+
   private
 
   # Overwriting the sign_out redirect path method
