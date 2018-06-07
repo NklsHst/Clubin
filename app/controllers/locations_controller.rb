47f50dcaf12563506b1bc41b9cb9dd1fc324a295
Niklas Heist <niklas.heist@online.de>
2018-06-07 12:26:04 +0200

    minor fix

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -57,7 +57,7 @@ before_action :authenticate_user!
         lng: @location.longitude,
         # label: "#{location.calculate_average_atmosphere.to_s}/#{location.calculate_average_queue.to_s}",
         infoWindow: { content: render_to_string(partial: "/locations/map_box_two", locals: { location: location }) },
-        icon: ActionController::Base.helpers.asset_path('club.png')
+        icon: ActionController::Base.helpers.asset_path('pin.png')
       }
     @markers << @marker
   end
