d928c31cd16e5bd1fc9a3bf41e3cb7884d6dd945
Kai Kuenne <Mail@kaikuenne.com>
2018-06-07 14:59:47 +0200

    sign out actually signs out now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
+    raise
    root_path
   end
 end
